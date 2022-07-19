6a0185c41b4444cc04a67ab287a2b1944fff271e
Brian Harsha <harshabc4@gmail.com>
2022-07-19 19:38:26 -0400

    cors anywhere

--- client/src/components/PersonList.js
+++ client/src/components/PersonList.js
@@ -1,25 +1,3 @@
-// import React, { useEffect } from "react";
-// import axios from "axios";
-
-// export default function PersonList() {
-//   const [state, setState] = React.useState({ persons: [] });
-
-//   useEffect(() => {
-//     axios.get(`https://jsonplaceholder.typicode.com/users`).then((res) => {
-//       const persons = res.data;
-//       setState({ persons });
-//     });
-//   });
-
-//   return (
-//     <ul>
-//       {state.persons.map((person) => (
-//         <li key={person.id}>{person.name}</li>
-//       ))}
-//     </ul>
-//   );
-// }
-
 import React from "react";
 import axios from "axios";
 
@@ -29,16 +7,19 @@ export default class PersonList extends React.Component {
   };
 
   componentDidMount() {
-    // axios.get(`https://jsonplaceholder.typicode.com/users`).then((res) => {
-    axios
-      .get(`https://api.websitecarbon.com/site?url=google.com`)
-      .then((res) => {
-        const persons = res.data;
-        this.setState({ persons });
-      });
+    axios.get(`https://jsonplaceholder.typicode.com/users`).then((res) => {
+      const persons = res.data;
+      this.setState({ persons });
+    });
   }
 
   render() {
-    return <ul>{this.state.persons}</ul>;
+    return (
+      <ul>
+        {this.state.persons.map((person) => (
+          <li key={person.id}>{person.name}</li>
+        ))}
+      </ul>
+    );
   }
 }
