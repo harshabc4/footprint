a0640afc50affc6793bd9088806579e77e77a829
Brian Harsha <harshabc4@gmail.com>
2022-07-19 12:02:31 -0400

    no cors

--- routes/carbon.js
+++ routes/carbon.js
@@ -8,5 +8,6 @@ const { getCarbon } = require("../controllers/carbon");
 
 // api routes
 router.get("/carbon", getCarbon);
+// router.get("https://api.websitecarbon.com/site?url=google.com", getCarbon);
 
 module.exports = router;
