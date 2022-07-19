de07c92c47243fb0f65717fc2893638cab54cd3e
Brian Harsha <harshabc4@gmail.com>
2022-07-19 09:42:25 -0400

    comment out all api functions in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,24 @@ import { useState, useEffect } from "react";
 
 // functions
 // import { getTest } from "./functions/test";
-import { getCarbon } from "./functions/carbon";
+// import { getCarbon } from "./functions/carbon";
 
 function App() {
   // const [data, setData] = useState("Hello World!");
-  const [website, setWebsite] = useState("");
+  // const [website, setWebsite] = useState("");
 
-  useEffect(() => {
-    getCarbon()
-      .then((res) => {
-        setWebsite(res);
-        console.log(res);
-      })
-      .catch((err) => console.log(err + "asdf"));
-  }, []);
+  // useEffect(() => {
+  //   getCarbon()
+  //     .then((res) => {
+  //       setWebsite(res);
+  //       console.log(res);
+  //     })
+  //     .catch((err) => console.log(err));
+  // }, []);
 
   return (
     <div className="App">
-      <h1>{website.url}</h1>
+      {/* <h1>{website.url}</h1> */}
       <h2>asdf</h2>
       {/* <h2>{website}</h2> */}
       <Main />
