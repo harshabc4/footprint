b2bb522fb036e28843f1815bfcb0139d16c7e53d
Brian Harsha <harshabc4@gmail.com>
2022-07-19 11:00:00 -0400

    fix api url

--- client/src/functions/carbon.js
+++ client/src/functions/carbon.js
@@ -1,18 +1,15 @@
 export const getCarbon = async () => {
   try {
-    const res = await fetch(
-      "https://api.websitecarbon.com/site?url=google.com",
-      {
-        method: "GET",
-        headers: {
-          Accept: "applicaton/json",
-          "Access-Control-Allow-Headers": "*",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "*",
-          "Content-type": "application/json; charset=UTF-8",
-        },
-      }
-    );
+    const res = await fetch("site?url=google.com", {
+      method: "GET",
+      headers: {
+        Accept: "applicaton/json",
+        "Access-Control-Allow-Headers": "*",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "*",
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
     return await res.json();
   } catch (err) {}
 };
