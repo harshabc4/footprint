70c2fa2684c9584d186b194a65854664ae6ff5d9
Brian Harsha <harshabc4@gmail.com>
2022-07-19 10:55:45 -0400

    full api url

--- client/src/App.js
+++ client/src/App.js
@@ -4,30 +4,23 @@ import { useState, useEffect } from "react";
 
 // functions
 // import { getTest } from "./functions/test";
-// import { getCarbon } from "./functions/carbon";
+import { getCarbon } from "./functions/carbon";
 
 function App() {
   // const [data, setData] = useState("Hello World!");
   const [website, setWebsite] = useState({});
 
-  // useEffect(() => {
-  //   getCarbon()
-  //     .then((res) => {
-  //       console.log(res);
-  //       setWebsite(res);
-  //     })
-  //     .catch((err) => console.log(err));
-  // }, []);
-
   useEffect(() => {
-    fetch("https://api.websitecarbon.com/site?url=google.com")
-      .then((res) => res.json())
-      .then((data) => setWebsite(data));
+    getCarbon()
+      .then((res) => {
+        console.log(res);
+        setWebsite(res);
+      })
+      .catch((err) => console.log(err));
   }, []);
 
   return (
     <div className="App">
-      <h1>{website.data.url}</h1>
       <h2>asdf</h2>
       <Main />
     </div>
