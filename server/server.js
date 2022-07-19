22284c6451905ab5f9615ed335f8efdfc7f33127
Brian Harsha <harshabc4@gmail.com>
2022-07-19 08:56:12 -0400

    reorganize for heroku

--- server/server.js
+++ server/server.js
@@ -5,6 +5,19 @@ const cors = require("cors");
 
 require("dotenv").config();
 
+// TEST START
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
+// TEST END
+
 // app
 const app = express();
 
