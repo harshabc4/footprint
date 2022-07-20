dc7f8f573f5c76638b5a43c67d186c4cb14bc49d
Brian Harsha <harshabc4@gmail.com>
2022-07-20 15:15:00 -0400

    change back to build

--- server.js
+++ server.js
@@ -59,10 +59,10 @@ app.use("/", carbonRoutes);
 const path = require("path");
 
 // Step 1:
-app.use(express.static(path.resolve(__dirname, "./client/public")));
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 // Step 2:
 app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/public", "index.html"));
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 
 // TEST END
